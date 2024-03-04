1f367e7a806c48b16537ef7e95810c7f7503ee63
Laisa Pereira <laisadsilva.pereira@gmail.com>
2024-03-03 21:11:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="/user" method="post">
+        
+          <label for="firstName">Nome:</label>
+          <input type="text" id="firstName" name="firstName"/>
+          <label for="lastName">Sobrenome:</label>
+          <input type="text" id="lastName" name="lastName"/>
+          <input type="submit" value="Submit"/>
+      </form>
+     
+        
     </div>
   );
 }
